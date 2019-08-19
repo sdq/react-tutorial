a74be8a0ad095c328e969b760823992cc9a2ffe3
sdq <sdq@tongji.edu.cn>
2019-08-19 20:26:00 +0800

    add demo3

---
+++ demo-3/src/Demo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Demo extends Component {
+
+    getGreeting(user) {
+        if (user) {
+          return <h1>Hello, {user}!</h1>;
+        }
+        return <h1>Hello, Stranger.</h1>;
+    }
+
+    render() {
+        return (
+            <div>
+                {this.getGreeting()}
+                {this.getGreeting("Jack")}
+            </div>
+        )
+    }
+}
