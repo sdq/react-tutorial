518b62be7c56e098ee38f078cc7bf604fc0d1dbc
sdq <sdq@tongji.edu.cn>
2019-09-11 21:04:55 +0800

    update to part 4

--- js-demo/src/Part03.js
+++ js-demo/src/Part03.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
+function defaultParam() {
+    function f(x, y = 5, z) {
+        return [x, y, z];
+    }
+    alert(f()) // [undefined, 5, undefined]
+    // alert(f(1)) // [1, 5, undefined]
+    // alert(f(1, ,2)) // error
+    // alert(f(1, undefined, 2)) // [1, 5, 2]
+}
+
+function restParam() {
+    function add(...values) {
+        let sum = 0;
+      
+        for (var val of values) {
+          sum += val;
+        }
+      
+        return sum;
+    }
+      
+    alert(add(2, 5, 3))
+}
+
+function arrowFunction() {
+    var sum = (num1, num2) => { return num1 + num2; }
+    alert(sum(10,5));
+}
+
 function Part03() {
     return (
         <div>
             <h2>3. function</h2>
+            <ol>
+                <li>
+                    <button onClick={defaultParam}>default parameter</button>
+                </li>
+                <li>
+                    <button onClick={restParam}>rest parameter</button>
+                </li>
+                <li>
+                    <button onClick={arrowFunction}>arrow function</button>
+                </li>
+            </ol>
         </div>
     );
 }
--- js-demo/src/Part04.js
+++ js-demo/src/Part04.js
@@ -1,9 +1,66 @@
 import React from 'react';
 
+function spread() {
+    function add(x, y) {
+        return x + y;
+    }
+    
+    const numbers = [4, 38];
+    alert(add(...numbers)) // 42
+}
+
+function concatArray() {
+    const arr1 = ['a', 'b'];
+    const arr2 = ['c'];
+    const arr3 = ['d', 'e'];
+    alert([...arr1, ...arr2, ...arr3]);
+}
+
+function spreadString() {
+    alert([...'hello']);
+}
+
+function findDemo() {
+    let result = [1, 5, 10, 15].find(function(value, index, arr) {
+        return value > 9;
+    })
+    alert(result);
+}
+
+function fillDemo() {
+    let result = new Array(3).fill(7);
+    alert(result);
+}
+
+function flatDemo() {
+    let result = [1, 2, [3, 4]].flat();
+    alert(result);
+}
+
 function Part04() {
     return (
         <div>
             <h2>4. array</h2>
+            <ol>
+                <li>
+                    <button onClick={spread}>spread</button>
+                </li>
+                <li>
+                    <button onClick={concatArray}>concat array</button>
+                </li>
+                <li>
+                    <button onClick={spreadString}>spread string</button>
+                </li>
+                <li>
+                    <button onClick={findDemo}>find() demo</button>
+                </li>
+                <li>
+                    <button onClick={fillDemo}>fill() demo</button>
+                </li>
+                <li>
+                    <button onClick={flatDemo}>flat() demo</button>
+                </li>
+            </ol>
         </div>
     );
 }
