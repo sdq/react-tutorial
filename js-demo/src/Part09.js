14f5820570a16deb94a66d0c1abeca59ad3c2644
sdq <sdq@tongji.edu.cn>
2019-09-11 23:36:36 +0800

    update 9 10

--- js-demo/src/Part09.js
+++ js-demo/src/Part09.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
+class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    toString() {
+      return '(' + this.x + ', ' + this.y + ')';
+    }
+
+    static classMethod() {
+        return 'this is class method';
+    }
+}
+
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color;
+    }
+  
+    toString() {
+        return this.color + ' ' + super.toString();
+    }
+}
+
+function classDemo() {
+    let p = new Point(2,3);
+    alert(p.toString());
+}
+
+function staticMethod() {
+    alert(Point.classMethod());
+}
+
+function classExtends() {
+    let cp = new ColorPoint(2,3, 'red');
+    alert(cp.toString());
+}
+
 function Part09() {
     return (
         <div>
             <h2>9. Class</h2>
+            <ol>
+                <li>
+                    <button onClick={classDemo}>class demo</button>
+                </li>
+                <li>
+                    <button onClick={staticMethod}>static method</button>
+                </li>
+                <li>
+                    <button onClick={classExtends}>class extend</button>
+                </li>
+            </ol>
         </div>
     );
 }
--- js-demo/src/Part10.js
+++ js-demo/src/Part10.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { firstName, lastName, year } from './profile';
+import myFunction from './profileDefault';
+
+function importDemo() {
+    alert(firstName + ' ' + lastName + ' ' + year);
+}
+
+function exportDefaultDemo() {
+    alert(myFunction(2, 3));
+}
 
 function Part10() {
     return (
         <div>
             <h2>10. Module</h2>
+            <ol>
+                <li>
+                    <button onClick={importDemo}>export and import</button>
+                </li>
+                <li>
+                    <button onClick={exportDefaultDemo}>export default</button>
+                </li>
+            </ol>
         </div>
     );
 }
